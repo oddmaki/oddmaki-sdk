9cfbd38f2a3dc363c060a1252e96345b6f89bb2f
Carlos <crevelop@gmail.com>
2026-03-30 15:52:00 -0500

    chore: update subgraph URL

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const CONTRACT_ADDRESSES = {
     diamond: '0x5067a8175086e6b4863660fa165f8605302781e7' as Address,
     conditionalTokens: '0x7364747372Ac4a175B5326f5B2C9CB1C271d32e8' as Address,
     usdc: '0xd7a0a331f6fa862222049c97a3dded97bed2ef93' as Address,
-    subgraph: 'https://api.studio.thegraph.com/query/1716020/octopus-protocol/version/latest',
+    subgraph: 'https://api.studio.thegraph.com/query/1716020/oddmaki/version/latest',
   }
 } as const;
 
