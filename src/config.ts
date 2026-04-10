dbda554d8f791bdd986822e73bfd7814dfc0d5d3
Carlos <crevelop@gmail.com>
2026-04-10 15:58:54 -0500

    chore: update to v0.14.0

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,9 @@ import { baseSepolia } from 'viem/chains';
 
 export const CONTRACT_ADDRESSES = {
   [baseSepolia.id]: {
-    diamond: '0x2612a6949a4eeafa0bc39a7d82e0c926d4180d99' as Address,
+    diamond: '0x188563069e0ae7247f0e2f0fce0382f0ed28d31a' as Address,
     conditionalTokens: '0x7364747372Ac4a175B5326f5B2C9CB1C271d32e8' as Address,
-    usdc: '0x643a4ec97528c719bbaa5457806d6898321df7d4' as Address,
+    usdc: '0xb7e73d2848dd908a90a50ba679719eb9375c3fdf' as Address,
     subgraph: 'https://api.studio.thegraph.com/query/1716020/oddmaki/version/latest',
   },
 } as const;
